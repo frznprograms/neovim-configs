ec290dc34ea71cca0adc71c11df41d128a5f47a3
frznprograms <shanevbharathan@gmail.com>
2025-08-09 22:06:49 +0800

    feat: refactored keymaps.lua - now includes on_attach function as best practice - allows for use of special warning suppression for jupyter notebooks

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,73 +1,94 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-local keymap = vim.keymap
-local opts = { noremap = true, silent = true }
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+local on_attach = function(buffnr)
+  local keymap = vim.keymap
+  local opts = { noremap = true, silent = true, buffer = buffnr }
 
--- delete a word backwards
-keymap.set("n", "dw", "vb_d")
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 
--- select all
-keymap.set("n", "<C-a>", "gg<S-v>G")
+  -- delete a word backwards
+  vim.keymap.set("n", "dw", "vb_d")
 
--- jumplist
-keymap.set("n", "<C-m>", "<C-i>", opts)
+  -- select all
+  vim.keymap.set("n", "<C-a>", "gg<S-v>G")
 
--- new tab
-keymap.set("n", "te", ":tabedit", opts)
--- tab navigation
-keymap.set("n", "<tab>", ":tabnext<CR>", opts)
-keymap.set("n", "<s-tab>", ":tabprev<CR>", opts)
+  -- jumplist
+  vim.keymap.set("n", "<C-m>", "<C-i>", opts)
 
--- Yank to system clipboard
-keymap.set({ "n", "v" }, "<leader>y", '"+y', { desc = "Yank to system clipboard" })
-keymap.set("n", "<leader>Y", '"+Y', { desc = "Yank line to system clipboard" })
+  -- new tab
+  vim.keymap.set("n", "te", ":tabedit", opts)
+  -- tab navigation
+  vim.keymap.set("n", "<tab>", ":tabnext<CR>", opts)
+  vim.keymap.set("n", "<s-tab>", ":tabprev<CR>", opts)
 
--- Paste from system clipboard
-keymap.set({ "n", "v" }, "<leader>p", '"+p', { desc = "Paste from system clipboard" })
-keymap.set({ "n", "v" }, "<leader>P", '"+P', { desc = "Paste before from system clipboard" })
+  -- Yank to system clipboard
+  vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', { desc = "Yank to system clipboard" })
+  vim.keymap.set("n", "<leader>Y", '"+Y', { desc = "Yank line to system clipboard" })
 
--- resize windows
-keymap.set("n", "<A-left>", "<C-w>>")
-keymap.set("n", "<A-right>", "<C-w><")
-keymap.set("n", "<A-up>", "<C-w>+")
-keymap.set("n", "<A-down>", "<C-w>-")
+  -- Paste from system clipboard
+  vim.keymap.set({ "n", "v" }, "<leader>p", '"+p', { desc = "Paste from system clipboard" })
+  vim.keymap.set({ "n", "v" }, "<leader>P", '"+P', { desc = "Paste before from system clipboard" })
 
--- diagnostic
-keymap.set("n", "<C-j>", function()
-  vim.diagnostic.jump({ count = 1, float = true })
-end, opts)
+  -- resize windows
+  vim.keymap.set("n", "<A-left>", "<C-w>>")
+  vim.keymap.set("n", "<A-right>", "<C-w><")
+  vim.keymap.set("n", "<A-up>", "<C-w>+")
+  vim.keymap.set("n", "<A-down>", "<C-w>-")
 
--- molten basic keymaps
-vim.keymap.set("n", "<localleader>m", ":MoltenEvaluateOperator<CR>", { desc = "evaluate operator", silent = true })
-vim.keymap.set(
-  "n",
-  "<localleader>os",
-  ":noautocmd MoltenEnterOutput<CR>",
-  { desc = "open output window", silent = true }
-)
+  -- diagnostic
+  vim.keymap.set("n", "<C-j>", function()
+    vim.diagnostic.jump({ count = 1, float = true })
+  end, opts)
 
--- additional molten keymaps
-vim.keymap.set("n", "<localleader>rr", ":MoltenReevaluateCell<CR>", { desc = "re-eval cell", silent = true })
-vim.keymap.set(
-  "v",
-  "<localleader>r",
-  ":<C-u>MoltenEvaluateVisual<CR>gv",
-  { desc = "execute visual selection", silent = true }
-)
-vim.keymap.set("n", "<localleader>oh", ":MoltenHideOutput<CR>", { desc = "close output window", silent = true })
-vim.keymap.set("n", "<localleader>md", ":MoltenDelete<CR>", { desc = "delete Molten cell", silent = true })
+  -- molten basic keymaps
+  vim.keymap.set("n", "<localleader>m", ":MoltenEvaluateOperator<CR>", { desc = "evaluate operator", silent = true })
+  vim.keymap.set(
+    "n",
+    "<localleader>os",
+    ":noautocmd MoltenEnterOutput<CR>",
+    { desc = "open output window", silent = true }
+  )
 
--- quarto preview keymap
-local quarto = require("quarto")
-vim.keymap.set("n", "<leader>qp", quarto.quartoPreview, { silent = true, noremap = true })
+  -- additional molten keymaps
+  vim.keymap.set("n", "<localleader>rr", ":MoltenReevaluateCell<CR>", { desc = "re-eval cell", silent = true })
+  vim.keymap.set(
+    "v",
+    "<localleader>r",
+    ":<C-u>MoltenEvaluateVisual<CR>gv",
+    { desc = "execute visual selection", silent = true }
+  )
+  vim.keymap.set("n", "<localleader>oh", ":MoltenHideOutput<CR>", { desc = "close output window", silent = true })
+  vim.keymap.set("n", "<localleader>md", ":MoltenDelete<CR>", { desc = "delete Molten cell", silent = true })
 
-local runner = require("quarto.runner")
-vim.keymap.set("n", "<localleader>rc", runner.run_cell, { desc = "run cell", silent = true })
-vim.keymap.set("n", "<localleader>ra", runner.run_above, { desc = "run cell and above", silent = true })
-vim.keymap.set("n", "<localleader>rA", runner.run_all, { desc = "run all cells", silent = true })
-vim.keymap.set("n", "<localleader>rl", runner.run_line, { desc = "run line", silent = true })
-vim.keymap.set("v", "<localleader>r", runner.run_range, { desc = "run visual range", silent = true })
-vim.keymap.set("n", "<localleader>RA", function()
-  runner.run_all(true)
-end, { desc = "run all cells of all languages", silent = true })
+  -- quarto preview keymap
+  local quarto = require("quarto")
+  vim.keymap.set("n", "<leader>qp", quarto.quartoPreview, { silent = true, noremap = true })
+
+  local runner = require("quarto.runner")
+  vim.keymap.set("n", "<localleader>rc", runner.run_cell, { desc = "run cell", silent = true })
+  vim.keymap.set("n", "<localleader>ra", runner.run_above, { desc = "run cell and above", silent = true })
+  vim.keymap.set("n", "<localleader>rA", runner.run_all, { desc = "run all cells", silent = true })
+  vim.keymap.set("n", "<localleader>rl", runner.run_line, { desc = "run line", silent = true })
+  vim.keymap.set("v", "<localleader>r", runner.run_range, { desc = "run visual range", silent = true })
+  vim.keymap.set("n", "<localleader>RA", function()
+    runner.run_all(true)
+  end, { desc = "run all cells of all languages", silent = true })
+end
+
+-- remove annoying warning at the end of jupyter notebooks
+require("lspconfig")["pyright"].setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    python = {
+      analysis = {
+        diagnosticSeverityOverrides = {
+          reportUnusedExpression = "none",
+        },
+      },
+    },
+  },
+})
