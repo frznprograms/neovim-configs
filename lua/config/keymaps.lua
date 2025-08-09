d90fa580a7b790ed1b1bc768e09bdeda944eae28
frznprograms <shanevbharathan@gmail.com>
2025-08-09 21:48:23 +0800

    feat: added dependencies for molten.nvim - added quarto plugin and new ftplugin folder - ftplugin will control when quarto is activate to help with markdown   files and nested code blocks
    feat: updated keymaps.lua
    - updated keymaps with quarto and molten keymaps
    
    fix: update lazy-lock.json

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -37,3 +37,37 @@ keymap.set("n", "<A-down>", "<C-w>-")
 keymap.set("n", "<C-j>", function()
   vim.diagnostic.jump({ count = 1, float = true })
 end, opts)
+
+-- molten basic keymaps
+vim.keymap.set("n", "<localleader>m", ":MoltenEvaluateOperator<CR>", { desc = "evaluate operator", silent = true })
+vim.keymap.set(
+  "n",
+  "<localleader>os",
+  ":noautocmd MoltenEnterOutput<CR>",
+  { desc = "open output window", silent = true }
+)
+
+-- additional molten keymaps
+vim.keymap.set("n", "<localleader>rr", ":MoltenReevaluateCell<CR>", { desc = "re-eval cell", silent = true })
+vim.keymap.set(
+  "v",
+  "<localleader>r",
+  ":<C-u>MoltenEvaluateVisual<CR>gv",
+  { desc = "execute visual selection", silent = true }
+)
+vim.keymap.set("n", "<localleader>oh", ":MoltenHideOutput<CR>", { desc = "close output window", silent = true })
+vim.keymap.set("n", "<localleader>md", ":MoltenDelete<CR>", { desc = "delete Molten cell", silent = true })
+
+-- quarto preview keymap
+local quarto = require("quarto")
+vim.keymap.set("n", "<leader>qp", quarto.quartoPreview, { silent = true, noremap = true })
+
+local runner = require("quarto.runner")
+vim.keymap.set("n", "<localleader>rc", runner.run_cell, { desc = "run cell", silent = true })
+vim.keymap.set("n", "<localleader>ra", runner.run_above, { desc = "run cell and above", silent = true })
+vim.keymap.set("n", "<localleader>rA", runner.run_all, { desc = "run all cells", silent = true })
+vim.keymap.set("n", "<localleader>rl", runner.run_line, { desc = "run line", silent = true })
+vim.keymap.set("v", "<localleader>r", runner.run_range, { desc = "run visual range", silent = true })
+vim.keymap.set("n", "<localleader>RA", function()
+  runner.run_all(true)
+end, { desc = "run all cells of all languages", silent = true })
